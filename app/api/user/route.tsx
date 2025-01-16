5a7b1602d9cde37a8954f81a766c55a79f68d499
Arindam Keswani <arindamkeswani@gmail.com>
2025-01-17 00:43:16 +0530

    Added Boilerplate, loader, default API routing

---
+++ app/api/user/route.tsx
@@ -0,0 +1,15 @@
+export function GET() {
+    //db logic
+    return Response.json({
+        email: "arindam@gmail.com",
+        name: "Arindam",
+    })
+}
+
+export function POST() {
+    //db logic
+    return Response.json({
+        email: "arindam@gmail.com",
+        name: "Arindam",
+    })
+}
