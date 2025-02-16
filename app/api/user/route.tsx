92812ad01e7a4d060b71c8c509d2b124866bb914
Arindam Keswani <arindamkeswani@gmail.com>
2025-02-16 19:38:19 +0530

    prisma init

--- app/api/user/route.tsx
+++ app/api/user/route.tsx
@@ -1,3 +1,8 @@
+import { PrismaClient } from "@prisma/client";
+import { NextRequest } from "next/server"
+
+const dbClient = new PrismaClient();
+
 export function GET() {
     //db logic
     return Response.json({
@@ -6,10 +11,30 @@ export function GET() {
     })
 }
 
-export function POST() {
-    //db logic
-    return Response.json({
-        email: "arindam@gmail.com",
-        name: "Arindam",
-    })
+export async function POST(req: NextRequest) {
+    try{
+
+        const body = await req.json();
+        //db logic
+    
+        const user = await dbClient.user.create({
+            data: {
+                username: body.username,
+                password: body.password
+            }
+        })
+    
+        return Response.json({
+            message: "Logged-in",
+            user: {
+                id: user.id
+            }
+        })
+    }
+    catch(e: any) {
+        return Response.json({
+            message: `Error: ${e.message}`,
+        })
+    }
 }
+
